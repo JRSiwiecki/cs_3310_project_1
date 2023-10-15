07f65001f0483d8e1bad4cc3b44a2bbfe3fa2908
JRSiwiecki <JRSiwiecki@gmail.com>
2023-10-15 12:35:39 -0700

    Create outline for function calls for next algos

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from classical_matrix_multiplication import classical_matrix_multiplication
 from utils import generate_random_matrices, print_matrix
+from classical_matrix_multiplication import classical_matrix_multiplication
 
-n = 3
-min = 0
-max = 5
+N = 3
+MIN = 0
+MAX = 5
 
-a, b = generate_random_matrices(n, [min, max])
+a, b = generate_random_matrices(N, [MIN, MAX])
+
 print_matrix(a)
 print_matrix(b)
+
 print_matrix(classical_matrix_multiplication(a, b))
+# print_matrix(divide_and_conquer_matrix_multiplication(a, b))
+# print_matrix(strassen_matrix_multiplication(a, b))
